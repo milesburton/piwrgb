611efd23990db556c8061c5fe5786ee855c8c9e3
Miles Burton <miles@milesburton.com>
2024-06-14 10:35:52 +0100

    Removed service

--- update_and_restart.sh
+++ update_and_restart.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 REPO_DIR="~/piwrgb"
-SERVICE_NAME="piwrgb_service"
 
 cd $REPO_DIR
 
@@ -13,14 +12,9 @@ REMOTE=$(git rev-parse @{u})
 if [ $LOCAL != $REMOTE ]; then
     echo "Changes detected. Updating repository."
 
-    # Stop the running service
-    sudo systemctl stop $SERVICE_NAME
-
     # Pull the latest changes
     git pull
 
-    # Restart the service
-    sudo systemctl start $SERVICE_NAME
 else
     echo "No changes detected."
 fi
